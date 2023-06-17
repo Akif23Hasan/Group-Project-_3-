95df7efbeb14b983da235faf05698a77059db223
NyxTM <mt370098256@gmail.com>
2023-06-17 13:36:49 +1000

    EDM_V1

---
+++ EDM/QuickDBD_V1.sql
@@ -0,0 +1,88 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+DROP TABLE IF EXISTS "4_Sector_Stocks";
+DROP TABLE IF EXISTS "FAANG";
+DROP TABLE IF EXISTS "T_test_AAPL";
+DROP TABLE IF EXISTS "T_test_AMZN";
+DROP TABLE IF EXISTS "T_test_FB";
+DROP TABLE IF EXISTS "T_test_GOOG";
+DROP TABLE IF EXISTS "T_test_GOOGL";
+DROP TABLE IF EXISTS "T_test_NFLX";
+
+
+
+CREATE TABLE "4_Sector_Stocks" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "Start_price" FLOAT   NOT NULL,
+    "End_price" FLOAT   NOT NULL,
+    "Pct_change" FLOAT   NOT NULL,
+    "Sector_pct_change" FLOAT   NOT NULL,
+    "Stock_trade_volume" FLOAT   NOT NULL,
+    "Trade_dollar_volume" FLOAT   NOT NULL,
+    "GICS_Sector" VARCHAR(100)   NOT NULL
+);
+
+CREATE TABLE "FAANG" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "Start_price" FLOAT   NOT NULL,
+    "End_price" FLOAT   NOT NULL,
+    "Pct_change" FLOAT   NOT NULL,
+    "Sector_pct_change" FLOAT   NOT NULL,
+    "Stock_trade_volume" FLOAT   NOT NULL,
+    "Trade_dollar_volume" FLOAT   NOT NULL
+);
+
+CREATE TABLE "T_test_AAPL" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "GICS_Sector" VARCHAR(100)   NOT NULL,
+    "t_value" FLOAT   NOT NULL,
+    "p_value" FLOAT   NOT NULL
+);
+
+CREATE TABLE "T_test_AMZN" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "GICS_Sector" VARCHAR(100)   NOT NULL,
+    "t_value" FLOAT   NOT NULL,
+    "p_value" FLOAT   NOT NULL
+);
+
+CREATE TABLE "T_test_FB" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "GICS_Sector" VARCHAR(100)   NOT NULL,
+    "t_value" FLOAT   NOT NULL,
+    "p_value" FLOAT   NOT NULL
+);
+
+CREATE TABLE "T_test_GOOG" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "GICS_Sector" VARCHAR(100)   NOT NULL,
+    "t_value" FLOAT   NOT NULL,
+    "p_value" FLOAT   NOT NULL
+);
+
+CREATE TABLE "T_test_GOOGL" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "GICS_Sector" VARCHAR(100)   NOT NULL,
+    "t_value" FLOAT   NOT NULL,
+    "p_value" FLOAT   NOT NULL
+);
+
+CREATE TABLE "T_test_NFLX" (
+    "Stock_symbol" VARCHAR(10)   NOT NULL,
+    "GICS_Sector" VARCHAR(100)   NOT NULL,
+    "t_value" FLOAT   NOT NULL,
+    "p_value" FLOAT   NOT NULL
+);
+
+
+
+
+SELECT * FROM "4_Sector_Stocks";
+SELECT * FROM "FAANG";
+SELECT * FROM "T_test_AAPL";
+SELECT * FROM "T_test_AMZN";
+SELECT * FROM "T_test_FB";
+SELECT * FROM "T_test_GOOG";
+SELECT * FROM "T_test_GOOGL";
+SELECT * FROM "T_test_NFLX";
