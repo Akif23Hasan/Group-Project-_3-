f10c7d8b450f674bb94ac356405ee62815b7f984
Grezza-78 <gmeadon@gmail.com>
2023-06-17 14:39:06 +1000

    working flask

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
 import os
 from flask import Flask, jsonify
+import json
+from flask_cors import CORS
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/data', methods=['GET'])
 def get_data():
+    
+    with open("Four_Sector_Stocks.json") as json_file:
+        data = json.load(json_file)
+    return jsonify(data)
+    
+    
+    
+    
     # Get the absolute path to the JSON file
-    file_path = os.path.join(os.path.dirname(__file__), 'static', 'js', 'DataFiles', '4_Sector_Stocks.json')
+    # file_path = os.path.join(os.path.dirname(__file__), 'static', 'js', 'DataFiles', 'Four_Sector_Stocks.json')
 
-    # Check if the JSON file exists
-    if os.path.exists(file_path):
-        # Read the JSON data from the file
-        with open(file_path, 'r') as file:
-            data = file.read()
+    # # Check if the JSON file exists
+    # if os.path.exists(file_path):
+    #     # Read the JSON data from the file
+    #     with open(file_path, 'r') as file:
+    #         data = file.read()
 
-        # Remove newline characters from the JSON data
-        data = data.replace('\n', '')
+    #     # Remove newline characters from the JSON data
+    #     data = data.replace('\n', '')
 
-        # Return the JSON data as the API response
-        return jsonify(data)
-    else:
-        return 'Data not found', 404
+    #     # Return the JSON data as the API response
+    #     return jsonify(data)
+    # else:
+    #     return 'Data not found', 404
 
 
 if __name__ == '__main__':
--- static/js/app.js
+++ static/js/app.js
@@ -1,87 +1,158 @@
 // Update the file path to the local JSON file
-const filePath = "DataFiles/4_Sector_Stocks.json";
+const URL = "http://127.0.0.1:5000/data";
 
-// Fetch the JSON data from the local file and console log it
-d3.json(filePath).then(function(data) {
-  console.log(data);
-});
 
-// Create an array of each country's numbers
-let AAPL = Object.values(FAANG.AAPL);
-let AMZN = Object.values(FAANG.AMZN);
-let NFLX = Object.values(FAANG.NFLX);
-let GOOG = Object.values(FAANG.GOOG);
-let GOOGL = Object.values(FAANG.GOOGL);
-let FB = Object.values(FAANG.FB);
 
-// Create an array of category labels
-let labels = Object.keys(FAANG.AAPL);
 
-// Display the default plots
+
+
+
+
+
+
+
+
+
+
+// Creating the function to design the Bubble Chart using two variables passed from Option Changed function
+function demo_Box(subject_id, data_set) {
+
+  // Filtering the id data to match select data received and assigning it to a variable
+  var meta_data = data_set.metadata.filter(row => row.id == subject_id);
+
+  // Selecting the sample_metadata from the HTML and then displaying the data by calling the demodata_Display function
+  // and adjusting the text size
+  d3.select("#sample-metadata").html(demodata_Display(meta_data[0]))
+                               .style("font-size","70%");
+}
+
+// Creating the function to extract the key and value paid from the passed meta_data returning it to 
+// it to the demo_Box function for displaying 
+function demodata_Display(meta_data) {
+  var string_data = "";
+  Object.entries(meta_data).forEach(([key,value]) => {
+      string_data += `<br>${key}:${value}</br>`;
+
+  });
+  return string_data;
+}
+
+// Creating the initiation function
 function init() {
-  let pieData = [{
-    values: AAPL.pct_change,
-    labels: labels,
-    type: "pie"
-  }];
+  
+  // Creating a selector to select the first instance in the data
+  let selector = d3.select("#selDataset");
+  
+  // Using D3 to read the URL and then jsonify the data for processing
+  d3.json(URL).then((data) => {
+      
+      // Assigning the data to a variable to be passed to all functions called
+      data_set = data;
+    
+      // Displaying the data_set for review
+      console.log(data_set)
 
-  let lineData = [{
-    x: labels,
-    y: AAPL,
-    type: "line"
-  }];
+      // Assigning the name to the subject_id that will be passed to all functions called 
+      let name = [];
 
-  let layout = {
-    height: 600,
-    width: 500
-  };
-
-  Plotly.newPlot("pie", pieData, layout);
-  Plotly.newPlot("line", lineData, layout);
+      if (data_set == 'Apple') {
+        name = "AAPL";
+      }
+      else if (data_set == 'Facebook(Meta)') {
+        name = "FB";
+      }
+      else if (data_set == 'Amazon') {
+        name = "AMZN";
+      }
+      else if (data_set == 'Google (COOG)') {
+        name = "GOOG";
+      }
+      else if (data_set == 'Google (GOOGL)') {
+        name = "GOOGL";
+      }
+      else if (data_set == 'Netflix') {
+        name = "NFLX";
+      }
+            
+      const subject_ids = data.name;
+      
+      console.log(subject_ids);
+      // // Displaying data in selector function
+      // subject_ids.forEach(id => {
+      //     selector.append("option")
+      //             .attr("value", id)
+      //             .text(id);
+      // });
+      // // Calling the Option Changed function as identified in HTML passing through the subject_id data name 
+      // optionChanged(subject_ids[0]); 
+  })    
 }
 
-// On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", getData);
 
-// Function called by DOM changes
-function getData() {
-  let dropdownMenu = d3.select("#selDataset");
-  // Assign the value of the dropdown menu option to a variable
-  let dataset = dropdownMenu.property("value");
-  // Initialize an empty array for the country's data
-  let data = [];
+// Creating the Option Changed function
+function optionChanged(subject_id) {
+    
+  // Calling the Bar Chart function passing through the subject_id Name and JSON data_set
+  bar_Chart(subject_id, data_set);
+  
+  // Calling the Bubble Chart function passing through the subject_id Name and JSON data_set
+  bubble_Chart(subject_id, data_set);
+  
+  // Calling the Demo Chart function passing through the subject_id Name and JSON data_set
+  demo_Box(subject_id, data_set);
 
-  if (dataset == 'AAPL') {
-    data = AAPL;
-  }
-  else if (dataset == 'AMZN') {
-    data = AMZN;
-  }
-  else if (dataset == 'NFLX') {
-    data = NFLX;
-  }
-  else if (dataset == 'GOOG') {
-    data = GOOG;
-  }
-  else if (dataset == 'GOOGL') {
-    data = GOOGL;
-  }
-  else if (dataset == 'FB') {
-    data = FB;
-  }
-  // Call function to update the charts
-  updatePlotly(data);
+  // Calling the Demo Chart function passing through the subject_id Name and JSON data_set
+  gauge_Chart(subject_id, data_set);
+  
 }
 
-// Update the restyled plots' values
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
-
-  let lineUpdate = {
-    y: [newdata]
-  };
-
-  Plotly.update("line", lineUpdate);
-}
-
+// Calling the init function that initialises the display
 init();
+
+
+
+
+
+
+
+// // Function called by DOM changes
+// function getData() {
+//   let dropdownMenu = d3.select("#selDataset");
+//   // Assign the value of the dropdown menu option to a variable
+//   let dataset = dropdownMenu.property("value");
+//   // Initialize an empty array for the country's data
+//   let data = [];
+
+//   if (dataset == 'AAPL') {
+//     data = AAPL;
+//   }
+//   else if (dataset == 'AMZN') {
+//     data = AMZN;
+//   }
+//   else if (dataset == 'NFLX') {
+//     data = NFLX;
+//   }
+//   else if (dataset == 'GOOG') {
+//     data = GOOG;
+//   }
+//   else if (dataset == 'GOOGL') {
+//     data = GOOGL;
+//   }
+//   else if (dataset == 'FB') {
+//     data = FB;
+//   }
+//   // Call function to update the charts
+//   updatePlotly(data);
+// }
+
+// // Update the restyled plots' values
+// function updatePlotly(newdata) {
+//   Plotly.restyle("pie", "values", [newdata]);
+
+//   let lineUpdate = {
+//     y: [newdata]
+//   };
+
+//   Plotly.update("line", lineUpdate);
+// }
+
